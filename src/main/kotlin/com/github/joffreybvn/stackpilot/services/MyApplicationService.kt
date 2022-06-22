aea7a37d0ad976ff578ad32f01fff9f749a51d92
GitHub Action <action@github.com>
2022-06-22 16:08:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/joffreybvn/stackpilot/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/joffreybvn/stackpilot/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.joffreybvn.stackpilot.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.joffreybvn.stackpilot.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/joffreybvn/stackpilot/services/MyProjectService.kt
+++ src/main/kotlin/com/github/joffreybvn/stackpilot/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.joffreybvn.stackpilot.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.joffreybvn.stackpilot.MyBundle
 
 class MyProjectService(project: Project) {
 
